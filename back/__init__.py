43f035dc7a13662e3bb6aa05f2e3308e495a7c87
hollaPuppy <roki216@mail.ru>
2021-09-03 21:27:21 +0300

    Deploy9

--- back/__init__.py
+++ back/__init__.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 
 from flask import Flask
@@ -6,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from back.settings import DATABASE_URL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://faozuispekgops:49e44fdedf916d54c5d562385a7677a6f387af7111403befac06fa0b2f96c73b@ec2-176-34-116-203.eu-west-1.compute.amazonaws.com:5432/d1gec1g9j9v7sr'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=300)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
