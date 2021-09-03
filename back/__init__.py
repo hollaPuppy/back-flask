f6f14ff1e7d9f964910da46030a5df98ba45519e
hollaPuppy <roki216@mail.ru>
2021-09-03 20:53:13 +0300

    Deploy4

--- back/__init__.py
+++ back/__init__.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-from settings import DATABASE_URL
+from back.settings import DATABASE_URL
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
